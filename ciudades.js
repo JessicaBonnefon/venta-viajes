b37437d4076f42d0b3337614369e1565277518d7
Jessica Bonnefon <jessbonne97@gmail.com>
2023-08-02 14:02:47 -0300

    agregamos javascript al proyecto de ventas

---
+++ ciudades.js
@@ -0,0 +1,34 @@
+const barcelona = {
+    titulo: 'dsjcnkfdfvdfbdnv',
+    precio: 'El precio promocional es de: $300',
+    subtitulo: 'kjsdkadbdfb fd bjbcf',
+    parrafo: 'sdkfjcbdfbhc'
+}
+
+const roma = {
+    titulo: 'fgbgsfgb',
+    precio: 'El precio promocional es de: $560',
+    subtitulo: 'kjsdkgfb gffgnmadjbcf',
+    parrafo: 'sdkfjcbn ghn bdfbhc'
+}
+
+const paris = {
+    titulo: 'dsjcndvafdbgfbfgbkdnv',
+    precio: 'El precio promocional es de: $230',
+    subtitulo: 'kjssfbsfdkadjbcf',
+    parrafo: 'sdkfdbsgfbsfjcbdfbhc'
+}
+
+const londres = {
+    titulo: 'fdvfgbdfbdsjcnkdnv',
+    precio: 'El precio promocional es de: $410',
+    subtitulo: 'kjfsbfdbfbfbsdfafsdkadjbcf',
+    parrafo: 'sdkfjcdvgbsdbdfbhc'
+}
+
+export {
+    barcelona,
+    roma,
+    paris,
+    londres
+};
